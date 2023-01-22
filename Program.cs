75e545458b4b6182d7940ef6756b6e60b49ee363
“Salil48” <salilchharia@gmail.com>
2023-01-22 19:54:36 +0530

    Welcome to Person Data Management

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace PraticingLambdaExpression
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Person Data Management");
+        }
+    }
+}
