b8b7d9e42a93aefedd984ff5207a6180c23e271f
“Salil48” <salilchharia@gmail.com>
2023-01-22 20:33:10 +0530

    Create Person class

---
+++ Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonDataManagement
+{
+    public class Person
+    {
+        public string SSN;
+        public string Name;
+        public string City;
+        public int Age;
+        public Person(string ssn, string name, string city, int age)
+        {
+            SSN = ssn;
+            Name = name;
+            City = city;
+            Age = age;
+        }
+
+    }
+}
